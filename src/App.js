4ecf0df10053cf71bbb81dacacfc0fe81c241053
Fatih Gören <fatihgoren61@gmail.com>
2022-03-10 21:37:52 +0300

    jsx template sample

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './template-Gourmet-Catering/Footer';
+import Header from './template-Gourmet-Catering/Header';
+import Navbar from './template-Gourmet-Catering/Navbar';
+import PageContent from './template-Gourmet-Catering/PageContent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <Header></Header>
+      <PageContent></PageContent>
+      <Footer></Footer>
+    </>
   );
 }
 
